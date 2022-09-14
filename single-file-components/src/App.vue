0a4e731266d511d24c8f0a9285b288547ad659a8
Geverson Loch <geverson.loch@uniodigital.com.br>
2022-09-14 17:45:06 -0300

    Add single-file-components

---
+++ single-file-components/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Hyperlink from './components/Hyperlink.vue';
+
+export default {
+  components: {
+    Hyperlink
+  }
+}
+</script>
+
+<template>
+  <h1>Single-File Components</h1>
+  <p>
+    Vue Single-File Components (também conhecido como arquivos *.vue, abreviado como SFC)<br>
+    é um formato de arquivo especial que nos permite encapsular o modelo, a lógica e o estilo<br>
+    de um componente Vue em um único arquivo.
+  </p>
+
+  <Hyperlink></Hyperlink>
+
+</template>
+
+<style>
+h1 {
+  color: #42B883;
+}
+</style>
